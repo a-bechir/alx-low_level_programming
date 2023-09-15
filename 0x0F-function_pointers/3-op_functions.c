9f6916cf6ad8538504141d519be2c0b086fdb763
a-bechir <abacheeer@gmail.com>
2023-09-15 01:22:05 -0700

    Updated

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
   * @a: int
   * @b: int
   *
-  * Return: int
+  * Return: a + b
   */
 int op_add(int a, int b)
 {
@@ -19,7 +19,7 @@ int op_add(int a, int b)
   * @a: int
   * @b: int
   *
-  * Return: int
+  * Return: a - b
   */
 int op_sub(int a, int b)
 {
@@ -31,7 +31,7 @@ int op_sub(int a, int b)
   * @a: int
   * @b: int
   *
-  * Return: int
+  * Return: a * b
   */
 int op_mul(int a, int b)
 {
@@ -43,7 +43,7 @@ int op_mul(int a, int b)
   * @a: int
   * @b: int
   *
-  * Return: int
+  * Return: a / b
   */
 int op_div(int a, int b)
 {
@@ -60,7 +60,7 @@ int op_div(int a, int b)
   * @a: int
   * @b: int
   *
-  * Return: int
+  * Return: a % b
   */
 int op_mod(int a, int b)
 {
